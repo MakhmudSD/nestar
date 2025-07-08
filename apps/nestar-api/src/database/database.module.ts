2bc60a41c7675d1838fba06991b7a20d2456e552
MakhmudSD <makhwork15@gmail.com>
2025-07-08 14:05:09 +0900

    fix: database updated

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
-import { privateDecrypt } from 'crypto';
 import { Connection } from 'mongoose';
 
 @Module({
